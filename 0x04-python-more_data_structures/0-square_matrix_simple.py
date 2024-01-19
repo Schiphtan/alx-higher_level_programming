8a9dcae5ad012f11d3442f1459220f2fdc0f056a
Staner <tuyumvire@gmail.com>
2024-01-18 19:06:41 -0800

    a function that computes the square value of all integers of a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
-
 """a function that computes the square value of all integers of a matrix"""
 
-def square_matrix_simple(matrix=[]):
 
-    # use list comprehension
+def square_matrix_simple(matrix=[]):
     new_matrix = [[x**2 for x in row] for row in matrix]
     return new_matrix
