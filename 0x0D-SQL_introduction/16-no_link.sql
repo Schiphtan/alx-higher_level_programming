bb7dd349ffae4ea4b88e38487f69768a0dd686b4
Staner <tuyumvire@gmail.com>
2024-02-19 06:33:17 -0800

    script that lists all records of the table second_table of the database

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,4 +2,5 @@
 -- Execute: cat 16-no_link.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
 SELECT score, name
 FROM second_table
-ORDER BY score DESC, name;
+WHERE name != '' OR name IS NOT NULL
+ORDER BY score DESC;
