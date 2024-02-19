d150c380d5a9e6c312c27809178717d6c261804d
Staner <tuyumvire@gmail.com>
2024-02-18 18:40:56 -0800

    Creates a database if not exists

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,6 @@
+/* a script that creates the database hbtn_0c_0 in my MySQL server
+
+   If the database hbtn_0c_0 already exists, my script should not fail
+ */
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0
