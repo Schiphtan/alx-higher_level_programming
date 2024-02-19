7176f87d4f6436229e410223783abc80f1d0030e
Staner <tuyumvire@gmail.com>
2024-02-19 01:19:08 -0800

    Write a script that creates a table second_table in the database hbtn_0c_0

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,10 @@
+-- Creates a table second_table
+-- Execute: cat 9-full_creation.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+
+-- Insert multiple rows into second_table
+INSERT INTO second_table (id, name, score) VALUES 
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
