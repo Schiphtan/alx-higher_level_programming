cb5e60c448d5870e4efb7e4a3c11864054aa35f5
Staner <tuyumvire@gmail.com>
2024-03-14 01:29:42 -0700

    script that creates the MySQL server user user_0d_1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,8 @@
--- a script that creates the MySQL server user user_01 with all privileges
+-- creates the MySQL server user user_01 with all privileges
 
 CREATE USER
 	IF NOT EXISTS 'user_0d_1'@'localhost'
 	IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILIGES
+GRANT ALL PRIVILEGES
 	ON *.*
-	TO 'user_0d_1'@'localhost'
-	IDENTIFIED BY 'localhost';
-FLUSH PRIVILEGES;
+	TO 'user_0d_1'@'localhost';
