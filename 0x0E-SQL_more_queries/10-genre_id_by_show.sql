edcdf71f3917625e4b89f9518c89345fe43628a5
Staner <tuyumvire@gmail.com>
2024-04-03 02:08:50 -0700

    script that lists all shows contained in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -4,4 +4,4 @@ SELECT s.`title`, g.`genre_id`
   FROM `tv_shows` AS s
         INNER JOIN `tv_show_genres` AS g
 	ON s.`id` = g.`show_id`
- ORDER BY s.`title`, g.`genre_id`;
+ ORDER BY s.`title`, g.`genre_id`cat 10-genre_id_by_show.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows;
